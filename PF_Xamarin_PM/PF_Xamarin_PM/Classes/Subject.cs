07c0ff8295f2e50d31fa551a09ac5a10737752a6
Lusecapi <sebastiancaicedo@uninorte.edu.co>
2017-11-22 20:09:40 -0500

    cambios menores

--- PF_Xamarin_PM/PF_Xamarin_PM/Classes/Subject.cs
+++ PF_Xamarin_PM/PF_Xamarin_PM/Classes/Subject.cs
@@ -11,14 +11,16 @@ namespace PF_Xamarin_PM
     {
         private string UId { get; set; } = null;
         public string Name { get; private set; }
-        public string ProfessorId { get; private set; } 
+        public string ProfessorId { get; private set; }
+        public string ProfessorFullName { get; private set; }
         public List<string> StudentsKeys { get; private set; } = new List<string>();
         public List<string> EvaluationsKeys { get; private set; } = new List<string>();
 
-        public Subject(string name, string professorId)
+        public Subject(string name, string professorFullName, string professorId)
         {
             Name = name;
             ProfessorId = professorId;
+            ProfessorFullName = professorFullName;
             UId = FirebaseHelper.GetNewUniqueID();
         }
 
